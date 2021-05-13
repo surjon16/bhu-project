b39018fb26301c1528128c43dc1c8bb5fb4e6e75
surjon07 <johnraymark.delpuerto@gmail.com>
2021-05-13 12:56:57 +0800

    Fix Env

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
 from application import app
 from data import db
 
-app.secret_key = 'blt_management_system'
+app.secret_key = 'bhu_management_system'
 
 if __name__ == '__main__':
     # db.drop_all()
     db.create_all()
-    app.run(debug=True, host='192.168.110.102', port='8080')
+    app.run(debug=True)#, host='192.168.110.102', port='8080')
--- web/data/__init__.py
+++ web/data/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from application import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/db_bhu'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/db_bhu' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Roannie:admin1234@Roannie.mysql.pythonanywhere-services.com/Roannie$db_bhu'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
